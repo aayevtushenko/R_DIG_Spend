f9552e6509811aefc8c56dc591a59cbd3b2739ff
Artem Y <aayevtushenko@gmail.com>
2017-11-20 10:23:52 -0500

    dig spend conversion file for free and cpmv
    Script for calculating free and cpmv. does not take a measurement for flat and the dates associated. takes a days running difference. can be split into weeks.

--- script/homegoods_spend.R
+++ script/homegoods_spend.R
@@ -1,28 +1,35 @@
-library(RDoubleClick)
-library(stringr)
-library(lubridate)
-library(dplyr)
-library(readr)
-library(readxl)
-library(openxlsx)
+# import files from dv, dcm and prisma and join them by using the pcodes. Then calculate the cost based on the cost method in prisma.
+# the scripts allow calculating single or multiple weekly spend
+# raw data are put in folder starts with "data_for"
+# output excel files are put into "DIG" folder
 
-client.id <- "420112744051-v16uovgvr58clj96cujohtfe1vernjrn.apps.googleusercontent.com"
-client.secret <- "J843SJWGUQtHmiT5fMXs2yGq"
+library(RDoubleClick) # package for dcm
+library(stringr) # for string mainipulation
+library(lubridate) # for date manipulation
+library(dplyr) # for data cleaning
+library(readr) # for reading files
+library(readxl) # for reading xlsx files
+library(openxlsx) # for writing excel files
+
+# DCN API: get access by using client.id and client.secret
+client.id <- "963284085963-6tehp3a30ovmjuk3v4bohg2t16no79pj.apps.googleusercontent.com"
+client.secret <- "hwatT9yHYdTZBnoGgNCVBS2J"
 
 DCAuth(client.id,client.secret,runwebserver = FALSE)
 
-
+# import double verify 
 dv_homegoods <- read_csv("data_for_hg/dv_homegoods.csv") %>% 
   mutate(Date = mdy(Date))
 
+# import prisma files
 prisma_homegood_1 <-read_excel("data_for_hg/prisma_homegood.xlsx", sheet = "q4") %>% 
-  mutate(p_code = substr(Name,1, 6),
+  mutate(p_code = substr(Name,1, 6), # get the pcode
          `Flight start date` = ymd(`Flight start date`),
          `Flight end date` = ymd(`Flight end date`),
          flight_week_diff = abs(week(`Flight end date`) - week(`Flight start date`)))
 
 prisma_homegood_2 <-read_excel("data_for_hg/prisma_homegood.xlsx", sheet = "2h") %>% 
-  mutate(p_code = substr(Name,1, 6),
+  mutate(p_code = substr(Name,1, 6), # get the pcode
          `Flight start date` = ymd(`Flight start date`),
          `Flight end date` = ymd(`Flight end date`),
          flight_week_diff = abs(week(`Flight end date`) - week(`Flight start date`)))
@@ -31,23 +38,22 @@ prisma_homegood_2 <-read_excel("data_for_hg/prisma_homegood.xlsx", sheet = "2h")
 prisma_homegood <- rbind(prisma_homegood_2, prisma_homegood_1)
 
 
-## Marshals data
-### download DCM report
+## download DCM report, reports needs to be built on DCM site. ONly problems for this R package
 dcm_homegood_reports <-  files.get(fileId=635138357,reportId = 108705867)
 
 dcm_homegood_reports_cleaned <- dcm_homegood_reports %>% 
-  mutate(p_code = substr(Placement,1, 6),
+  mutate(p_code = substr(Placement,1, 6), # get the pcodes
          Week = week(Date)) %>% 
   rename("DCM_clicks" = Clicks,
-         "DCM_impressions" = Impressions) %>% 
-  group_by(p_code, Week) %>% 
+         "DCM_impressions" = Impressions) %>%  # rename columns
+  group_by(p_code, Week) %>%   # sum based on p_code and week (sumifs in excel)
   summarise(DCM_clicks = sum(DCM_clicks),
             DCM_impressions= sum(DCM_impressions)) %>% 
-  select(Week,p_code,DCM_clicks,DCM_impressions)
+  select(Week,p_code,DCM_clicks,DCM_impressions) # keep relevant columns
 
 ### double verify
 dv_homegood_cleaned <- dv_homegoods %>% 
-  mutate(p_code = substr(`Placement Name`,1, 6),
+  mutate(p_code = substr(`Placement Name`,1, 6), # get pcode
          Week = week(Date)) %>% 
   select(`Campaign Name`,`Media Property`,`Placement Name`,Date,Week,p_code,`GroupM Billable Impressions`) %>% 
   group_by(p_code, Week) %>% 
@@ -61,6 +67,9 @@ prisma_homegood_cleaned<-prisma_homegood %>%
   select(p_code,Supplier, `Cost method` ,Rate, `Total Planned Cost`,
          # oct_cost_by_week, 
          flight_week_diff)
+
+# loops to go combine homegood_dcm_dc and prisma_cleaned data based on the weeks in the former dataset. Only keep relevant pcodes' data that are 
+#  present in prisma_cleaned data. Then calculate based on "cost method" column 
 
 homegood_dig_spend <- data.frame()
 
---
+++ script/homegoods_spend.py
@@ -0,0 +1,124 @@
+import pandas as pd
+
+def safe_div(x,y):
+    if y == 0:
+        return x
+    return x / y
+
+# to be used in apply function
+# cost method is already passed    
+def applyCalculateSpend (df_name, cost_method_col, metric_col, rate_col, total_planned_col):
+    calculations = {
+            'CPMV'  : df_name[metric_col] / 1000 * df_name[rate_col],
+            'Free'  : 0
+            }
+    df_method = df_name[cost_method_col]
+    return calculations.get(df_method, "not in dict")
+
+'''
+# USED FOR TESTS
+test_df = pd.read_csv('data_for_hg/prisma_test.csv')
+
+test_df['spend'] = test_df.apply(lambda row: applyCalculateSpend(
+row,
+cost_method_col='cost method',
+metric_col='metric',
+rate_col='rate',
+total_planned_col='total planned'), axis = 1)
+'''
+
+'''
+# Use the where method
+test_df['Spend'] = np.where(test_df['cost method'] == 'CPMV', test_df.metric/1000 * test_df.rate,
+           np.where(test_df['cost method'] == 'Flat', 5,0))
+'''
+
+# Open all tables
+dcm_hg = pd.read_csv('data_for_hg/dcm_homegoods_Sep-Oct.csv', header=10)
+dv_hg = pd.read_csv('data_for_hg/dv_homegoods.csv')
+prisma_hg = pd.read_excel('data_for_hg/prisma_homegoods.xlsx', sheetname='2h')
+
+# DCM
+dcm_hg['PCode'] = dcm_hg['Package/Roadblock'].str[:6] # get PCode
+dcm_hg['Date'] = pd.to_datetime(dcm_hg['Date']) # get date format
+dcm_hg['Week'] = dcm_hg['Date'].dt.week # get week
+dcm_hg = dcm_hg.iloc[:,3:] #Drop the left three cols
+
+dcm_hg = dcm_hg.groupby(['PCode', 'Week'])['Impressions', 'Clicks'].sum() #.reset_index() # group by pcode & week
+
+# DV
+dv_hg['PCode'] = dv_hg['Placement Name'].str[:6] # get pcode
+dv_hg = dv_hg[['Media Property', 'PCode','Date', 'GroupM Billable Impressions']] # select relevant cols
+dv_hg['Date'] = pd.to_datetime(dv_hg['Date']) # set date to a datetime format
+dv_hg['Week'] = dv_hg['Date'].dt.week # get the week number from the datetime format
+dv_hg = dv_hg.groupby(['PCode','Week'])['GroupM Billable Impressions'].sum() #.reset_index() # Becomes series
+dv_hg = pd.DataFrame(dv_hg)
+
+# Prisma
+prisma_hg = prisma_hg[['Campaign flight start date',
+              'Campaign flight end date',
+              'Flight start date',
+              'Flight end date',
+              'Supplier',
+              'Placement number',
+              'Cost method',
+              'Rate',
+              'Total Planned Cost']] # get relevant items from the date time format 
+prisma_hg['prisma_flight_length_hg'] = abs(prisma_hg['Flight end date'] - prisma_hg['Flight start date'])
+prisma_hg = prisma_hg.fillna(0) # fill nans as 0s
+
+# Full Outer Join dv and dcm
+dcm_dv_hg = pd.merge(dcm_hg, dv_hg, left_index=True, right_index=True, how='outer').reset_index().fillna(0)
+
+# loop to GO combine dcm_dv_hg and prisma_hg based on the weeks in the former dataset.
+# Only keep relevant pcodes' data that are present in prisma_cleaned data.
+# Then calculate based on "cost method" column
+
+# variable declaration
+dig_spend = None
+
+for i in dcm_dv_hg['Week'].unique():
+    weekly_dcm_dv_hg = dcm_dv_hg.loc[dcm_dv_hg['Week'] == i]
+
+    #Indenting for testing. Unindent and remove if statement
+#    if i == 40:
+    # left join prisma and dcm_dv_loop item "weekly_dcm_dv_hg" on the PCode
+    data = pd.merge(prisma_hg, weekly_dcm_dv_hg, left_on='Placement number', right_on='PCode', how='left')
+    data['spend'] = data.apply(lambda row: applyCalculateSpend(
+        row,
+        cost_method_col='Cost method',
+        metric_col='GroupM Billable Impressions',
+        rate_col='Rate',
+        total_planned_col='Total Planned Cost'), axis = 1)
+    data = data.groupby(['Supplier']).agg({'spend': 'sum', 'Week': 'min'}).reset_index()
+    # Unionize
+    dig_spend = pd.concat([dig_spend, data])
+        
+dig_spend.to_csv('dig_spend.csv', index=False)
+
+print('\n'*3)
+print("""███╗   ███╗██╗███╗   ██╗██████╗ ███████╗██╗  ██╗ █████╗ ██████╗ ███████╗
+████╗ ████║██║████╗  ██║██╔══██╗██╔════╝██║  ██║██╔══██╗██╔══██╗██╔════╝
+██╔████╔██║██║██╔██╗ ██║██║  ██║███████╗███████║███████║██████╔╝█████╗  
+██║╚██╔╝██║██║██║╚██╗██║██║  ██║╚════██║██╔══██║██╔══██║██╔══██╗██╔══╝  
+██║ ╚═╝ ██║██║██║ ╚████║██████╔╝███████║██║  ██║██║  ██║██║  ██║███████╗
+╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝
+                                                                        
+███╗   ███╗ █████╗ ██████╗ ███████╗ ██████╗██╗                          
+████╗ ████║██╔══██╗██╔══██╗██╔════╝██╔════╝██║                          
+██╔████╔██║███████║██████╔╝███████╗██║     ██║                          
+██║╚██╔╝██║██╔══██║██╔══██╗╚════██║██║     ██║                          
+██║ ╚═╝ ██║██║  ██║██║  ██║███████║╚██████╗██║                          
+╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝ ╚═════╝╚═╝                          
+                                                                        
+""")
+print('Total spend is %f' % (dig_spend.sum(numeric_only=True)['spend']))
+    
+    
+    
+    
+    
+    
+    
+    
+    
